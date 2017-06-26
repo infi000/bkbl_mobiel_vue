b2ef4ae2a0f3542941a52c02845add45caca0bff
infiooo <113079767@qq.com>
2017-06-26 18:44:09 +0800

    2ed

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
  * @Author: 张驰阳
  * @Date:   2017-06-23 10:50:38
  * @Last Modified by:   张驰阳
- * @Last Modified time: 2017-06-23 17:34:47
+ * @Last Modified time: 2017-06-26 17:20:43
  */
 
 'use strict';
@@ -33,6 +33,7 @@ var app = new Vue({
     	demo:demo.data1,
 
     },
+    router,
     computed:{
     	barStyle:function(){
     		var h=this.demo.subject,
